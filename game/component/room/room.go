c8024898f0fbf35261507d3705ac16169566f85e
run <emailofrun@qq.com>
2026-01-18 04:07:57 +0800

    feat: 实现房间用户踢出与自动解散机制
    - Room 增加用户未准备踢出机制 ( 定时任务)。
    - 增加用户离开房间推送 () 及更新用户信息推送 ()。
    - 实现房间解散逻辑 ()：当房间无用户时，自动清理资源并通知 Union 移除房间引用。
    - 引入  接口，解决 Room 与 Union 的循环依赖。
    - 完善房间消息协议类型定义。

--- game/component/room/room.go
+++ game/component/room/room.go
@@ -1,21 +1,29 @@
 package room
 
 import (
+	"common/logs"
 	"core/models/entity"
 	"framework/msError"
 	"framework/remote"
+	"game/component/base"
 	"game/component/proto"
 	"game/component/sz"
 	"game/models/request"
+	"sync"
+	"time"
 )
 
 type Room struct {
-	Id          string
-	UnionID     int64
-	GameRule    proto.GameRule
-	users       map[string]*proto.RoomUser
-	RoomCreator *proto.RoomCreator
-	GameFrame   GameFrame
+	sync.RWMutex
+	Id            string
+	UnionID       int64
+	GameRule      proto.GameRule
+	users         map[string]*proto.RoomUser
+	RoomCreator   *proto.RoomCreator
+	GameFrame     GameFrame
+	KickSchedules map[string]*time.Timer
+	union         base.UnionBase
+	dismissed     bool
 }
 
 // 1.推送房间号到客户端；2.推送游戏类型给客户端（进入游戏也要推送一次）
@@ -34,6 +42,7 @@ func (r *Room) UserEntryRoom(session *remote.Session, user *entity.User) *msErro
 	session.Put("roomId", r.Id)
 	// 2.推送游戏类型给客户端
 	r.SelfEntryRoomPush(session, user.Uid)
+	go r.addKickScheduleEvent(session, user.Uid)
 	return nil
 }
 
@@ -81,12 +90,74 @@ func (r *Room) GetRoomSceneInfoPush(session *remote.Session) {
 	session.Push([]string{session.GetUid()}, data, "ServerMessagePush")
 }
 
-func NewRoom(id string, unionID int64, rule proto.GameRule) *Room {
+func (r *Room) addKickScheduleEvent(session *remote.Session, uid string) {
+	r.KickSchedules[uid] = time.AfterFunc(10*time.Second, func() {
+		logs.Info("kick 执行，用户 %v 长时间未准备", uid)
+		// 取消定时任务，目前 time.AfterFunc 就是一次性的，也可以不停止
+		timer, ok := r.KickSchedules[uid]
+		if ok {
+			timer.Stop()
+		}
+		delete(r.KickSchedules, uid)
+		// 判断用户是否该踢出
+		user, ok := r.users[uid]
+		if ok && user.UserStatus < proto.Ready {
+			r.kickUser(user, session)
+			// 判断是否可以解散房间
+			if len(r.users) == 0 {
+				r.dismissRoom()
+			}
+		}
+	})
+}
+
+func (r *Room) ServerMessagePush(users []string, data any, session *remote.Session) {
+	// 通过 nats 发送消息到 connector，connector 推送消息到客户端
+	session.Push(users, data, "ServerMessagePush")
+}
+
+func (r *Room) kickUser(user *proto.RoomUser, session *remote.Session) {
+	kickUid := user.UserInfo.Uid
+	// 给被踢用户推送：roomId 为空的消息
+	r.ServerMessagePush([]string{kickUid}, proto.UpdateUserInfoPush(""), session)
+	// 通知房间其他用户
+	users := make([]string, len(r.users))
+	for uid, _ := range r.users {
+		users = append(users, uid)
+	}
+	r.ServerMessagePush(users, proto.UserLeaveRoomPushData(user), session)
+	delete(r.users, kickUid)
+}
+
+// 解散房间，将 union 存储的房间信息，删除掉
+func (r *Room) dismissRoom() {
+	r.Lock()
+	defer r.Unlock()
+	// 避免重复解散
+	if r.dismissed {
+		return
+	}
+	r.dismissed = true
+	r.cancelAllScheduler()
+	r.union.DismissRoom(r.Id)
+}
+
+// 取消所有定时任务
+func (r *Room) cancelAllScheduler() {
+	for uid, timer := range r.KickSchedules {
+		timer.Stop()
+		delete(r.KickSchedules, uid)
+	}
+}
+
+func NewRoom(id string, unionID int64, rule proto.GameRule, u base.UnionBase) *Room {
 	r := &Room{
-		Id:       id,
-		UnionID:  unionID,
-		GameRule: rule,
-		users:    make(map[string]*proto.RoomUser),
+		Id:            id,
+		UnionID:       unionID,
+		GameRule:      rule,
+		users:         make(map[string]*proto.RoomUser),
+		KickSchedules: make(map[string]*time.Timer),
+		union:         u,
 	}
 	if rule.GameType == int(proto.PinSanZhang) {
 		r.GameFrame = sz.NewGameFrame(rule, r)
--- game/logic/union.go
+++ game/logic/union.go
@@ -7,9 +7,11 @@ import (
 	"framework/remote"
 	"game/component/room"
 	"game/models/request"
+	"sync"
 )
 
 type Union struct {
+	sync.RWMutex
 	id    int64
 	m     *UnionManager
 	Rooms map[string]*room.Room
@@ -19,10 +21,16 @@ type Union struct {
 func (u *Union) CreateRoom(service *service.UserService, session *remote.Session, req request.CreateRoomReq, user *entity.User) *msError.Error {
 	// 1.创建房间
 	roomId := u.m.CreateRoomId()
-	newRoom := room.NewRoom(roomId, req.UnionID, req.GameRule)
+	newRoom := room.NewRoom(roomId, req.UnionID, req.GameRule, u)
 	u.Rooms[roomId] = newRoom
 	// 2.推送房间号到客户端
 	return newRoom.UserEntryRoom(session, user)
+}
+
+func (u *Union) DismissRoom(roomId string) {
+	u.Lock()
+	defer u.Unlock()
+	delete(u.Rooms, roomId)
 }
 
 func NewUnion(m *UnionManager) *Union {
